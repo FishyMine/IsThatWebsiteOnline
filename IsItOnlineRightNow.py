74acd0826ef3805d047f1b3363583634dbad2552
FishyMine <72989672+FishyMine@users.noreply.github.com>
2022-07-09 10:23:52 -0400

    Update IsItOnlineRightNow.py

--- IsItOnlineRightNow.py
+++ IsItOnlineRightNow.py
@@ -1,26 +1,68 @@
 # View the github here: https://github.com/FishyMine/IsThatWebsiteOnline
 # Fully made by FishyMine
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 try: 
   import time 
-  print("######## Is it online right now? v1.0.1 ########")
+  print(bcolors.OKBLUE + "######## Is it online right now? v1.0.1 ########")
   time.sleep(0.9)
   print("Want more? Add more stuff to this project! https://github.com/FishyMine/IsThatWebsiteOnline")
   time.sleep(0.9)
-  print("Please wait while the console installs all of the packages.")
+  print(bcolors.WARNING + "Please wait while the console installs all of the packages.")
   import urllib.request
   import socket
-  print("Please enter the website URL -- NO SPACES, NUMBERS, OR SYMBOLS (URL MUST BE VALID)")
+  import json
+  print(bcolors.BOLD + "Please enter the website URL -- NO SPACES, NUMBERS, OR SYMBOLS (URL MUST BE VALID)")
   print("DONT ADD HTTPS:// OR HTTP://")
+  
+  
   website = input()
-  print ("-------------------------PLEASE WAIT---------------------------")
+  print (bcolors.HEADER + "-------------------------PLEASE WAIT---------------------------")
+  print(bcolors.OKBLUE + "Checking...")
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   result = sock.connect_ex((website, 80))
-   
+  print(bcolors.OKBLUE + "10%")
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  result = sock.connect_ex((website, 80))
+  print(bcolors.OKBLUE + "20%")
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  result = sock.connect_ex((website, 80))
+  print(bcolors.OKBLUE + "30%")
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  result = sock.connect_ex((website, 80))
+  print(bcolors.OKBLUE + "40%")
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  result = sock.connect_ex((website, 80))
+  print(bcolors.OKBLUE + "50%")
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  result = sock.connect_ex((website, 80))
+  print(bcolors.OKBLUE + "60%")
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  result = sock.connect_ex((website, 80))
+  print(bcolors.OKBLUE + "70%")
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  result = sock.connect_ex((website, 80))
+  print(bcolors.OKBLUE + "80%")
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  result = sock.connect_ex((website, 80))
+  print(bcolors.OKBLUE + "90%")
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  result = sock.connect_ex((website, 80))
+  print(bcolors.OKBLUE + "100%")
 except:
-    print("This website is not available, suspended, or doesnt exist.")
-    exit(0)
+    print(bcolors.FAIL + "This website is not available, suspended, or doesnt exist.")
+    exit()
 if result == 0:
     
-   print ("Website is currently online and detected")
+   print (bcolors.OKGREEN + "Website is currently online and detected")
 else:
-   print ("Website is offline or not detected")
+   print (bcolors.FAIL + "Website is offline or not detected")
